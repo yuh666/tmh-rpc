d601b47056c39a0be84bf5e97b3f03101930f721
yuh666 <yuhao@zhuangyue.com>
2020-06-14 16:11:39 +0800

    qps

--- client-demo/src/main/java/show/tmh/rpc/demo/main/Main.java
+++ client-demo/src/main/java/show/tmh/rpc/demo/main/Main.java
@@ -1,17 +1,31 @@
 package show.tmh.rpc.demo.main;
 
 import show.tmh.rpc.client.core.RpcFactory;
-import show.tmh.rpc.demo.EchoService;
 import show.tmh.rpc.demo.UserService;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         UserService userService = RpcFactory.create(UserService.class);
+        CountDownLatch countDownLatch = new CountDownLatch(10);
         long l = System.currentTimeMillis();
-        for (int i = 0; i < 100000; i++) {
-            userService.get(1L);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 0; j < 100000; j++) {
+                        userService.get(1L);
+                    }
+                    countDownLatch.countDown();
+                }
+            });
         }
+        countDownLatch.await();
         System.out.println(System.currentTimeMillis() - l);
     }
 }
