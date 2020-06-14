de91471b631d5d803487cb428489b5a54596ebc0
yuh666 <yuhao@zhuangyue.com>
2020-06-14 15:56:06 +0800

    slow version

--- client-demo/src/main/java/show/tmh/rpc/demo/main/Main.java
+++ client-demo/src/main/java/show/tmh/rpc/demo/main/Main.java
@@ -9,10 +9,13 @@ public class Main {
     public static void main(String[] args) {
         EchoService echoService = RpcFactory.create(EchoService.class);
         UserService userService = RpcFactory.create(UserService.class);
-        for (int i = 0; i < 100; i++) {
-            System.out.println(echoService.echo("Hello World!"));
-            System.out.println(userService.get(1L));
+        long l = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            echoService.echo("Hello World!");
+            userService.get(1L);
         }
+
+        System.out.println(System.currentTimeMillis() - l);
 
 
     }
